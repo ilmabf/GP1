55403da989912a14f435826023f638af9298870e
BF Ilma <2019cs061@stu.ucsc.lk>
2021-09-28 22:32:41 +0530

    Manager upcoming reservations

--- app/views/managerReservationUpcoming.php
+++ app/views/managerReservationUpcoming.php
@@ -5,52 +5,52 @@
 <div style="min-height: 110px;"></div>
 
 <div class="heading">
-    <h2>Upcoming Reservations</h2>
+    <h2 >Upcoming Reservations</h2>
 </div>
 
 <div class="reservation-date">
-    <h3>Date</h3>
+    <h3 style="text-align: center; color:#085394;">Pick a Date</h3>
 
     <div id="closeClick">
         <button class="btnclss" onclick="closeOnClickDemo()">Click to open date picker</button>
-        <input type="text" id="date">
+        <input type="text3" id="date">
 
     </div>
 </div>
-
+<!-- <h2  style="text-align: center; color:#085394;">My Reservations</h3> -->
 <div class="upcomingOrders">
-    <h3>Upcoming Orders</h3>
+    
     <div class="sub-box1">
-        <div class="orderID">
-            <h3 class="a1">Order ID</h3>
-            <p class="a1 p1">AAAA</p>
+        <div class="order">
+            <div class="orderitem">Order ID</div>
+            <p class="orderitem1">AAAA</p>
         </div>
-        <div class="vehicleNo">
-            <h3 class="a2">Vecicle No</h3>
-            <p class="a2 p1">AD - 2234</p>
+        <div class="order">
+            <div class="orderitem">Vecicle No</div>
+            <p class="orderitem1">AD - 2234</p>
         </div>
-        <div class="orderTime">
-            <h3 class="a3">Time</h3>
-            <p class="a3 p1">8 am - 10 am</p>
+        <div class="order">
+            <div class="orderitem">Time</div>
+            <p class="orderitem1">8 am - 10 am</p>
         </div>
         <div class="orderView">
             <p class="viewLink"><a href="/manager/viewUpcomingOrder">View order</a></p>
-            <p class="team">Assigned Service Team A</p>
+            <p class="team2">Assigned Service Team A</p>
         </div>
     </div>
 
     <div class="sub-box1">
-        <div class="orderID">
-            <h3 class="a1">Order ID</h3>
-            <p class="a1 p1">BBBB</p>
+        <div class="order">
+            <div class="orderitem">Order ID</div>
+            <p class="orderitem1">BBBB</p>
         </div>
-        <div class="vehicleNo">
-            <h3 class="a2">Vecicle No</h3>
-            <p class="a2 p1">XZ - 2874</p>
+        <div class="order">
+            <div class="orderitem">Vecicle No</div>
+            <p class="orderitem1">XZ - 2874</p>
         </div>
-        <div class="orderTime">
-            <h3 class="a3">Time</h3>
-            <p class="a3 p1">10 am - 12 pm</p>
+        <div class="order">
+            <div class="orderitem">Time</div>
+            <p class="orderitem1">10 am - 12 pm</p>
         </div>
         <div class="orderView">
             <p class="viewLink"><a href="/manager/viewUpcomingOrder">View order</a></p>
@@ -59,17 +59,17 @@
     </div>
 
     <div class="sub-box1">
-        <div class="orderID">
-            <h3 class="a1">Order ID</h3>
-            <p class="a1 p1">CCCC</p>
+        <div class="order">
+            <div class="orderitem">Order ID</div>
+            <p class="orderitem1">CCCC</p>
         </div>
-        <div class="vehicleNo">
-            <h3 class="a2">Vecicle No</h3>
-            <p class="a2 p1">AR - 1142</p>
+        <div class="order">
+            <div class="orderitem">Vecicle No</div>
+            <p class="orderitem1">AR - 1142</p>
         </div>
-        <div class="orderTime">
-            <h3 class="a3">Time</h3>
-            <p class="a3 p1">2 pm - 4 pm</p>
+        <div class="order">
+            <div class="orderitem">Time</div>
+            <p class="orderitem1">2 pm - 4 pm</p>
         </div>
         <div class="orderView">
             <p class="viewLink"><a href="/manager/viewUpcomingOrder">View order</a></p>
