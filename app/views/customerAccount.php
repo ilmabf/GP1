217966f39830f54e477b99fe7ac6e2127938f354
Abdulla Nalim <2019cs001@stu.ucsc.lk>
2021-09-28 13:06:59 +0530

    customer and manager booking UIs

--- app/views/customerAccount.php
+++ app/views/customerAccount.php
@@ -2,13 +2,41 @@
   
     include 'userLoggedInHeader.php';
     $details = $_SESSION['userDetails'];
+    
 ?>
 
 <main>
-    <div id = "container">
+
+<div class="addVehicleform" id = "vehicleForm">
+                <div class="forma">
+                    <div class="loguser-icon"></div>
+                    <h2 class="login-signupheader">Add a vehicle</h2>
+
+                    <form action="/acount/addVehicle" method="post" id="customer-signup">
+                        
+                        <input class="input-box" id="vin" type="text2" name="vin" autofocus placeholder="Vehicle Identification No." required>
+                        <br>
+                        <input class="input-box" type="text2" name="model" autofocus placeholder="Model" required>
+                        <br>
+                        <input class="input-box" type="text2" name="color" autofocus placeholder="Color" required>
+                        <br>
+                        <input class="input-box" type="text2" name="type" autofocus placeholder="Type" required>
+                        <br>
+                        <input class="input-box" type="text2" name="manufacturer" autofocus placeholder="Manufacturer" required>
+                        <br>
+                        <button id="VehicleFormSubmitButton" class="formSubmitButton" type="submit" name="signup">Submit</button>
+                        <button id="VehicleFormCloseButton" class="formCancelButton" type="submit" name="signup" onclick="closeVehicleForm()">Cancel</button>
+                    </form>
+
+                </div>
+            </div>
+
+    <div id = "box"class = "box">
     <div class="account-box1">
+        <div class = "account-prof">
             <div class="account-header">My Account</div>
             <div class="account-box2">
+            <button class="btn">Edit</button>
                 <div class = "account-image">    </div>   
                 <div class = "account-details" >
                     <div class = "account-name">
@@ -21,9 +49,10 @@
                         <?php echo $details[0]['Contact_Number']; ?></div>
 
                 </div>
-            
+      
                 
-            </div><br>
+            </div>    </div> <br>
+        <div class = "account-det1">
             <div class="account-header2">My Vehicles</div>
             <div class="account-box3">
                 <div class = "vehicle-list">
@@ -33,10 +62,11 @@
                         <option value="Team B">JQ 942</option>
                     </select>
                 </div>
-                <button class = "button"> + Add </button>
+                <button class = "button" onclick="openVehicleForm()"> + Add </button>
             </div>
 
             <div class="vehicle-box">
+                <button class="btn2" >Edit</button>
                 <div class = "item1">
                     <div class = "vehicle">Model</div>
                     <div class = "vehicle-specs">
@@ -44,19 +74,43 @@
                     </div>
                 </div>
                 <div class = "item1">
-                <div class = "vehicle">Color</div>
-                <div class = "vehicle-specs">
-                    <?php echo "Blue"; ?>
-                </div>
+                    <div class = "vehicle">Color</div>
+                    <div class = "vehicle-specs">
+                        <?php echo "Blue"; ?>
+                    </div>
                 </div>
                 <div class = "item1">
-                <div class = "vehicle">Type</div>
-                <div class = "vehicle-specs">
-                    <?php echo "Car"; ?>
+                    <div class = "vehicle">Type</div>
+                    <div class = "vehicle-specs">
+                        <?php echo "Car"; ?>
+                    </div>
                 </div>
+                <div class = "item1">
+                    <div class = "vehicle">Manufacturer</div>
+                    <div class = "vehicle-specs">
+                        <?php echo "XYZ"; ?>
+                    </div>
                 </div>
+            </div><br>
+            </div>
+            <div class = "account-det2">
+            <div class="account-header2">My Locations</div>
+            <div class="account-box3">
+                <div class = "vehicle-list">
+                    <select name="Vehicles" id="Customer-Vehicles">
+                        <option value="select service team">No 5, 1st Road, City 05</option>
+                        <option value="Team A">No 12/A, 2nd Lane, City 01</option>
+                    </select>
+                </div>
+                <button class = "button"> + Add </button>
             </div>
 
+            <div class="map-box">
+            <button class="btn3">Edit</button>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.8229579296126!2d80.50619191725059!3d7.373730722101816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae341aee58e2aad%3A0xc15b721347b882fb!2sMalwathugoda%20Auto%20Service%20Station!5e0!3m2!1sen!2slk!4v1629713877312!5m2!1sen!2slk"  width = "100%" height = "100%"style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div><br>
+            </div>
+                
 </div>
 </div>
 
@@ -67,5 +121,5 @@
 ?>
 
 
-
+<script src="/public/js/customerAccount.js"></script> 
 </html>
