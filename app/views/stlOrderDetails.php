fe2b6a1f1d8aec68c6233602ff04115525b13e48
prasadani98 <nadeesha.aluthwaththa@gmail.com>
2021-10-01 16:49:53 +0530

    STL UIs: changes

--- app/views/stlOrderDetails.php
+++ app/views/stlOrderDetails.php
@@ -4,70 +4,68 @@
 <div style="min-height: 110px;"></div>
 <div>
     <h1 class="stl-orders-h1">Order Details</h1>
-    <div class="order-box">
 
-        <div class="viewOrder">
-            <div class="orderID">
-                <h3 class="a1 p1">Order ID</h3>
-                <p class="a1">AAAA</p>
+    <div class="order-box-1">
+
+        <div class="sub-box-1">
+
+            <div class="sub-box-2">
+                <div class="order-title">Customer Name</div>
+                <div class="order-description">Gihan Anthony</div>
+            </div>
+            <div class="sub-box-2">
+                <div class="order-title">Date</div>
+                <div class="order-description">2021/10/18</div>
+            </div>
+            <div class="sub-box-2">
+                <div class="order-title">Time</div>
+                <div class="order-description">8 am - 10 am</div>
+            </div>
+            <div class="sub-box-2">
+                <div class="order-title" >Contact No</div>
+                <div class="order-description">0775674896</div>
             </div>
         </div>
+        
 
-        <div class="viewOrder">
+        <div class="sub-box-1">
 
-            <div class="orderCustomer">
-                <h3 class="a2 p1">Customer Name</h3>
-                <p class="a2">Gihan Anthony</p>
+            <div class="sub-box-2">
+                <div class="order-title">Vehicle No</div>
+                <div class="order-description">AD - 2315</div>
             </div>
-            <div class="orderDate">
-                <h3 class="a7 p1">Date</h3>
-                <p class="a7">2021/10/18</p>
+            <div class="sub-box-2">
+                <div class="order-title">Model</div>
+                <div class="order-description">Axio</div>
             </div>
-            <div class="orderTime">
-                <h3 class="a7 p1">Time</h3>
-                <p class="a7">8 am - 10 am</p>
+            <div class="sub-box-2">
+                <div class="order-title">Vehicle Type</div>
+                <div class="order-description">Sedan</div>
             </div>
-            <div class="orderCustomer">
-                <h3 class="a3 p1">Contact No</h3>
-                <p class="a3">0775674896</p>
+            <div class="sub-box-2">
+                <div class="order-title">Color</div>
+                <div class="order-description">Black</div>
             </div>
         </div>
+        
 
-        <div class="viewOrder">
-            <div class="orderVehicle">
-                <h3 class="a4 p1">Vehicle No</h3>
-                <p class="a4">AD - 2315</p>
+        <div class="sub-box-1">
+            <div class="sub-box-2">
+                <div class="order-title" >Service Type</div>
+                <div class="order-description">Interior Cleaning</div>
             </div>
-            <div class="orderVehicle">
-                <h3 class="a5 p1">Model</h3>
-                <p class="a5">Axio</p>
+            <div class="sub-box-2">
+                <div class="order-title">Price</div>
+                <div class="order-description">1000</div>
             </div>
-            <div class="orderVehicle">
-                <h3 class="a6 p1">Vehicle Type</h3>
-                <p class="a6">Sedan</p>
-            </div>
-            <div class="orderVehicle">
-                <h3 class="a7 p1">Color</h3>
-                <p class="a7">Black</p>
+            <div class="sub-box-2">
+                <div class="order-title"><a href=" ">Location</a></div> 
             </div>
         </div>
-
-        <div class="viewOrder">
-            <div class="orderServiceType">
-                <h3 class="a7 p1">Service Type</h3>
-                <p class="a7">Interior Cleaning</p>
-            </div>
-            <div class="orderPrice">
-                <h3 class="a7 p1">Price</h3>
-                <p class="a7">1000</p>
-            </div>
-            <div class="orderLocation">
-                <h3 class="a7"><a href=" ">Location</a></h3> 
-            </div>
-        </div>
+        
     </div>
 
-    <div class="uploadImage" > 
+    <div class="upload-box" > 
         <form action="stlImageUpload.php" method="post" enctype="multipart/form-data">
             <div class="uploadImage" > <h3 class="a1 p1">Before service:</h3>
             <input type="file" name="beforeService" id="beforeService"></div>
@@ -77,11 +75,13 @@
         </form>
     </div>
 
-    <div style="flex:auto" class="prev-pg">
-            <button class="previous-button"><a href="/stl/viewAssignedOrders" style="color: white;">Back</a></button>
-   </div>
-   <div style="flex:auto"  class="next-pg">
-            <button class="next-button"><a href="/stl/stlHome" style="color: white;">Service Completed</a></button>
-   </div>
+        <div style="flex:auto" class="previous-pg">
+            <button class="back-button"><a href="/stl/viewAssignedOrders" style="color: white;">Back</a></button>
+        </div>
+
+        <div style="flex:auto"  class="next-page">
+            <button class="completed-button"><a href="/stl/stlHome" style="color: white;">Service Completed</a></button>
+        </div>
   
  </div>
+ 
