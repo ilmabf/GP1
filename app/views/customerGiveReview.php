f8f855cb062e1eb40bf88a9249bb84e330c2b964
BF Ilma <2019cs061@stu.ucsc.lk>
2021-10-04 23:42:22 +0530

    reviews backend

--- app/views/customerGiveReview.php
+++ app/views/customerGiveReview.php
@@ -11,8 +11,8 @@
             <div class="review-box1">
                     <div class="review-header">We need your feedback!</div>
                         <form action="/review/store" method="post" id="user-login" name="loginForm" >
-                            <div class = "review-box">
-                            <textarea name="review" placeholder="Give us a review.." required style = "height:200px" ></textarea>
+                            <div class = "Givereview-box">
+                            <textarea name="review" placeholder="Give us a review.." required style = "height:200px" maxlength="255"></textarea>
                             </div>
                             <button class="review-box-button" type="submit" name="login">Post Review</button>
                         </form>
