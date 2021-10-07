a4d4ca892c8da91b59509d817c3da7d42d38c8cd
Abdulla Nalim <2019cs001@stu.ucsc.lk>
2021-10-07 17:54:57 +0530

    controllers

--- app/views/adminManageEmployee.php
+++ app/views/adminManageEmployee.php
@@ -7,9 +7,13 @@
 
     <div style="min-height: 110px;"></div>
     <h2 class="manageEmployee-heading">Manage Employee</h2> 
-    <p>
-    <input type="button" id="addRow" value="Add Employee" onclick="addRow(); this.onclick=null;" />
-    </p>
+    <!-- <p> -->
+    <div class="addBtnEmps">
+      <input type="button" id="addRow" class="addTableEmp" value="Add Employee" onclick="addRow(); this.onclick=null;" />
+      <input type="button" id="addStlRow" class="addTableEmp" value="Add Service Team Leader" onclick="addStlRow(); this.onclick=null;" />
+    </div>
+    
+    <!-- </p> -->
     <form action="/employee/addNewEmployee" name="Form" method="post">
       <div id="cont"></div>  <!-- the container to add the TABLE -->
 
