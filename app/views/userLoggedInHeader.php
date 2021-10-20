fcddf7fbcb1583cdc3c491e673426f5e155b00ac
BF Ilma <2019cs061@stu.ucsc.lk>
2021-10-20 12:55:48 +0530

    css organized

--- app/views/userLoggedInHeader.php
+++ app/views/userLoggedInHeader.php
@@ -47,10 +47,10 @@
     <link rel="stylesheet" href="/public/css/actors/user/Login.css">
     <link rel="stylesheet" href="/public/css/actors/user/Reviews.css">
 
-    <!-- <link rel="stylesheet" href="/public/css/components/Forms.css">
+    <link rel="stylesheet" href="/public/css/components/Forms.css">
     <link rel="stylesheet" href="/public/css/components/Buttons.css">
 
-    <link rel="stylesheet" href="/public/css/Utilities.css"> -->
+    <link rel="stylesheet" href="/public/css/Utilities.css">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
