43eb8fedb867a1f3372885c41589feb6e81c6fc4
BF Ilma <2019cs061@stu.ucsc.lk>
2021-10-20 18:08:31 +0530

    password validation (reset and signup)

--- app/views/userChangePwd.php
+++ app/views/userChangePwd.php
@@ -11,11 +11,11 @@
                     <h2 class="login-signupheader">Enter New Password</h2>
 
                     <form action="/user/updatePassword" method="POST" autocomplete="off">
-                        <input class="input-box" id="new-pwd" type="password" name="new_pwd" placeholder="New Password" autocomplete="off" required>
+                        <input class="input-box" id="new-pwd" type="password" name="new_pwd" placeholder="New Password" autocomplete="off" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"  required>
                         <br>
                         <input class="input-box" id="new-confirm-pwd" type="password" name="re-enter_new_pwd" placeholder="ReEnter Password" autocomplete="off" required>
                         <br>
-                        <button class="input-box loggin-signup-button" type="submit" name="confirm_new_pwd">Confirm</button>
+                        <button class="input-box loggin-signup-button" id = "confirm-button" type="submit" name="confirm_new_pwd">Confirm</button>
                     </form>
 
                 </div>
