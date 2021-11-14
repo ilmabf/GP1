d8d6c3b126f2bf937a20dee3dc73d9c067ef365c
BF Ilma <2019cs061@stu.ucsc.lk>
2021-11-14 23:41:41 +0530

    geocode api

--- app/views/customer/Account.php
+++ app/views/customer/Account.php
@@ -8,7 +8,7 @@
 
     <div style="min-height: 110px;"></div>
     <div class="map" id="googleMapbox">
-    <h2 class="login-signupheader"><b>Add a location</b></h2>
+        <h2 class="login-signupheader"><b>Add a location</b></h2>
         <div id="map"></div>
         <div id="mapControl">
             <button id="AddLocationButton" class="formSubmitButton" type="submit" name="signup">Add</button>
@@ -211,84 +211,7 @@
     getVehicleDetails();
 </script>
 <script src="/public/js/Maps.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDxvVN9pPMljGjWLvUGWGisQwGUUMSOHco&callback=myMap"></script>
-<script type="text/javascript">
-    var infowindow;
-    var map;
-    var red_icon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
-    var myOptions = {
-        zoom: 7,
-        center: new google.maps.LatLng(46.87916, -3.32910),
-        mapTypeId: 'roadmap'
-    };
-    map = new google.maps.Map(document.getElementById('map'), myOptions);
-
-    var markers = {};
-
-    var getMarkerUniqueId = function(lat, lng) {
-        return lat + '_' + lng;
-    };
-
-    var getLatLng = function(lat, lng) {
-        return new google.maps.LatLng(lat, lng);
-    };
-
-    var addMarker = google.maps.event.addListener(map, 'click', function(e) {
-        var lat = e.latLng.lat(); // lat of clicked point
-        var lng = e.latLng.lng(); // lng of clicked point
-        var markerId = getMarkerUniqueId(lat, lng); // an that will be used to cache this marker in markers object.
-        var marker = new google.maps.Marker({
-            position: getLatLng(lat, lng),
-            map: map,
-            animation: google.maps.Animation.DROP,
-            id: 'marker_' + markerId,
-            html: "    <div id='info_" + markerId + "'>\n" +
-                "        <table class=\"map1\">\n" +
-                "            <tr><td></td><td><input type='button' value='Save' <a href = '/account/addLocation/" + lat + "/" + lng + "'</a>/></td></tr>\n" +
-                "        </table>\n" +
-                "    </div>"
-        });
-        markers[markerId] = marker; // cache marker in markers object
-        bindMarkerEvents(marker); // bind right click event to marker
-        bindMarkerinfo(marker); // bind infowindow with click event to marker
-    });
-
-    /**
-     * Binds  click event to given marker and invokes a callback function that will remove the marker from map.
-     * @param {!google.maps.Marker} marker A google.maps.Marker instance that the handler will binded.
-     */
-
-    var bindMarkerinfo = function(marker) {
-        google.maps.event.addListener(marker, "click", function(point) {
-            var markerId = getMarkerUniqueId(point.latLng.lat(), point.latLng.lng()); // get marker id by using clicked point's coordinate
-            var marker = markers[markerId]; // find marker
-            infowindow = new google.maps.InfoWindow();
-            infowindow.setContent(marker.html);
-            infowindow.open(map, marker);
-            // removeMarker(marker, markerId); // remove it
-        });
-    };
-
-    /**
-     * Binds right click event to given marker and invokes a callback function that will remove the marker from map.
-     * @param {!google.maps.Marker} marker A google.maps.Marker instance that the handler will binded.
-     */
-
-    var bindMarkerEvents = function(marker) {
-        google.maps.event.addListener(marker, "rightclick", function(point) {
-            var markerId = getMarkerUniqueId(point.latLng.lat(), point.latLng.lng()); // get marker id by using clicked point's coordinate
-            var marker = markers[markerId]; // find marker
-            removeMarker(marker, markerId); // remove it
-        });
-    };
-
-    /**
-     * Removes given marker from map.
-     * @param {!google.maps.Marker} marker A google.maps.Marker instance that will be removed.
-     * @param {!string} markerId Id of marker.
-     */
-    var removeMarker = function(marker, markerId) {
-        marker.setMap(null); // set markers setMap to null to remove it from map
-        delete markers[markerId]; // delete marker instance from markers object
-    };
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDxvVN9pPMljGjWLvUGWGisQwGUUMSOHco&callback=myMap&v=weekly"></script>
+<script async>
+    initMap();
 </script>
--- public/js/customerAccount.js
+++ public/js/customerAccount.js
@@ -116,3 +116,18 @@ function openMap() {
     x.style.display = "none";
   }
 }
+
+function closeMap() {
+  var x = document.getElementById("googleMapbox");
+  var z = document.getElementById("mapControl");
+  var y = document.getElementById("mainbox");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    z.style.display = "none";
+    y.classList.remove("blurAccount");
+  } else {
+    x.style.display = "block";
+    z.style.display = "block";
+  }
+}
+
