aaf90cf2bfeca3b947e869202ea75f439fd52ad2
prasadani98 <nadeesha.aluthwaththa@gmail.com>
2021-12-22 21:01:00 +0530

    Manager View selected past reservation-Changes

--- app/views/manager/CompletedReservations.php
+++ app/views/manager/CompletedReservations.php
@@ -1,7 +1,8 @@
 <?php
 include 'views/user/LoggedInHeader.php';
- $orderList = $_SESSION['completedReservations'];
+    $orderList = $_SESSION['completedReservations'];
 ?>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
 
 <div class="bgImage">
 
@@ -17,8 +18,8 @@ include 'views/user/LoggedInHeader.php';
         <div id="closeClick">
             <!-- <button class="btnclss" onclick="closeOnClickDemo()">Click to open calendar</button><br> -->
             <br>
-            <h3 style="color:white; text-shadow:1px 1px 4px #000000, 1px 1px 4px #0000fa;">Reservations on :</h3>
-            <input type="date" value="date" name="managerDateofCompletedBooking" class="dateBooking" onclick="viewList()" id="ManagerCompletedDate">
+            <div style="color:white; text-shadow:1px 1px 4px #000000, 1px 1px 4px #0000fa; display:inline;">Date :</div>
+            <input id="ManagerCompletedDate" type="text" name="managerDateofCompletedBooking" class="dateBooking" style="width: 50%;">
 
 
         </div>
@@ -38,6 +39,10 @@ include 'views/user/LoggedInHeader.php';
                     <div class="orderitem1"><?php echo $orderList[$count]['Vehicle_ID'] ?></div>
                 </div>
                 <div class="order">
+                    <div class="orderitem">Date</div>
+                    <div class="orderitem1"><?php echo $orderList[$count]['Date'] ?></div>
+                </div>
+                <div class="order">
                     <div class="orderitem">Time</div>
                     <div class="orderitem1"><?php echo $orderList[$count]['Time'] ?></div>
                 </div>
@@ -51,13 +56,65 @@ include 'views/user/LoggedInHeader.php';
           
         </div>
     </div>
+
     <div style="min-height: 110px;"></div>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
     <script>
 
     var orders = <?php echo json_encode($_SESSION['completedReservations']); ?>;
+    
+        $('#ManagerCompletedDate').datepicker({
+            dateFormat: 'yy-mm-dd',
+            onSelect: function(date) {
 
-    function viewList(){
+                document.getElementById("managerCompleteReservations").innerHTML = '';
+
+                var i = 0;
+                var list = [];
+
+                for (i = 0; i < orders.length; i++) {
+                    if (orders[i]['Date'] == date) {
+                        var order = [];
+                        order['Reservation_ID'] = orders[i]['Reservation_ID'];
+                        order['Vehicle_ID'] = orders[i]['Vehicle_ID'];
+                        order['Time'] = orders[i]['Time'];
+                        order['Service_team_leader_ID'] = orders[i]['Service_team_leader_ID'];
+                        list.push(order);
+                    }
+                }
+                var x = document.getElementById("managerCompleteReservations");
+
+                for (j = 0; j < list.length; j++) {
+                    
+                        x.innerHTML += "<div class='sub-box1' >" +
+                            "<div class='order'>" +
+                            "<div class='orderitem'>Order ID</div>" +
+                            "<div class='orderitem1'>" + list[j]['Reservation_ID'] + "</div>" +
+                            "</div>" +
+                            "<div class='order'>" +
+                            "<div class='orderitem'>Vehicle No</div>" +
+                            "<div class='orderitem1'>" + list[j]['Vehicle_ID'] + "</div>" +
+                            "</div>" +
+                            "<div class='order'>" +
+                            "<div class='orderitem'>Time</div>" +
+                            "<div class='orderitem1'>" + list[j]['Time'] + "</div>" +
+                            "</div>" +
+                            "<div class='orderView'>" +
+                            "<p class='viewLink'><a href='/booking/completedOrder/" + list[j]['Reservation_ID'] + "'>View order</a></p>" +
+                            "<p class='team'>Assigned Service Team " + list[j]['Service_team_leader_ID'] + "</p>" +
+                            "</div>" +
+                            "</div>";
+                    
+               }
+
+            }
+        });
+
+  </script>
+
+    <!--function viewList(){
 
         var d = document.getElementById("ManagerCompletedDate").value;
 
@@ -99,5 +156,5 @@ include 'views/user/LoggedInHeader.php';
            "</div>";
         }
     
-    }
-    </script>
+    }-->
+   
