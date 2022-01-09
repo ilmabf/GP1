cb5482a9764d7763424afa2d8ccc51d93f3b595c
unknown <2019cs061@stu.ucsc.lk>
2022-01-09 13:15:34 +0530

    address limit

--- public/js/Maps.js
+++ public/js/Maps.js
@@ -13,11 +13,57 @@ function saveLocation() {
   }
   Lattitude = latlng[0]["geometry"]["location"].lat();
   Longitude = latlng[0]["geometry"]["location"].lng();
-  window.location =
-    "/account/saveAddress/" + address + "/" + Lattitude + "/" + Longitude;
-  console.log(address);
-  window.location =
-    "/account/saveAddress/" + address + "/" + Lattitude + "/" + Longitude;
+
+  const serviceCentre = {
+    lat: 7.3732731971156165,
+    lng: 80.50720042798572,
+  };
+  const custAddress = {
+    lat: Lattitude,
+    lng: Longitude,
+  };
+
+  let directionsService = new google.maps.DirectionsService();
+
+  const route = {
+    origin: serviceCentre,
+    destination: custAddress,
+    travelMode: "DRIVING",
+  };
+
+  directionsService.route(route, function (response, status) {
+    // anonymous function to capture directions
+    if (status !== "OK") {
+      window.alert("Directions request failed due to " + status);
+      return;
+    } else {
+      // directionsRenderer.setDirections(response); // Add route to the map
+      var directionsData = response.routes[0].legs[0]; // Get data about the mapped route
+      if (!directionsData) {
+        window.alert("Directions request failed");
+        return;
+      } else {
+        var km = directionsData.distance.text;
+        kmInt = parseInt(km);
+        if (kmInt <= 50) {
+          window.location =
+            "/account/saveAddress/" +
+            address +
+            "/" +
+            Lattitude +
+            "/" +
+            Longitude;
+        } else {
+          var distanceMsg = "Out of limits";
+          console.log(distanceMsg);
+
+          limitDiv.style.display = "block";
+          limit.innerText = distanceMsg;
+          // responseDiv.appendChild(distanceMsg);
+        }
+      }
+    }
+  });
 }
 
 let map;
@@ -28,6 +74,8 @@ let response;
 var customerAddress = "";
 var Lattitude;
 var latlng;
+var kmInt;
+let limitDiv;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -64,11 +112,21 @@ function initMap() {
   responseDiv.id = "response-container";
   responseDiv.appendChild(response);
 
+  limit = document.createElement("lim");
+  limit.id = "limit";
+  limit.innerText = "";
+  limitDiv = document.createElement("div");
+  limitDiv.id = "limit-container";
+  limitDiv.style.fontSize = "xx-large";
+  limitDiv.style.backgroundColor = "white";
+  limitDiv.appendChild(limit);
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(limitDiv);
+
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputText);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitButton);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(clearButton);
   // map.controls[google.maps.ControlPosition.LEFT_TOP].push(instructionsElement);
-  map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
+  // map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
   marker = new google.maps.Marker({
     map,
   });
@@ -91,6 +149,7 @@ function initMap() {
 function clear() {
   marker.setMap(null);
   responseDiv.style.display = "none";
+  limitDiv.style.display = "none";
 }
 
 function geocode(request) {
@@ -103,7 +162,7 @@ function geocode(request) {
       map.setCenter(results[0].geometry.location);
       marker.setPosition(results[0].geometry.location);
       marker.setMap(map);
-      responseDiv.style.display = "block";
+      // responseDiv.style.display = "block";
       response.innerText = JSON.stringify(result, null, 2);
       console.log(results);
       for (var i = 1; i < results[0]["address_components"].length; i++) {
